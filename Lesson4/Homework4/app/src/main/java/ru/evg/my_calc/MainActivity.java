ae786a264593239a9fcc11972937fa64fe7e49f7
mrdowny <shikinea@mail.ru>
2017-04-10 00:15:27 +0300

    добавлены кнопки, но не выстроены

---
+++ Lesson4/Homework4/app/src/main/java/ru/evg/my_calc/MainActivity.java
@@ -0,0 +1,13 @@
+package ru.evg.my_calc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
